1c322ee5dcc4a9a6524ca9646dc91836c71aefa3
gitSilvia <154083502+gitSilvia@users.noreply.github.com>
2024-10-21 18:29:19 -0300

    Add files via upload

---
+++ Tarea 4/tarea4-1.php
@@ -0,0 +1,17 @@
+<?php
+$dia = 24; //se declara una variable de tipo integer.
+$sueldo = 758.43;//se declara una variable  de tipo double.
+$nombre = "UTIC";//se declara una variable de tipo String
+$exite = true;//se declara una variable de tipo boolean.
+echo "Valraible entera:";
+echo $dia;
+echo PHP_EOL;;
+echo"Variable double:";
+echo $sueldo;
+echo PHP_EOL;
+echo "Variable string:";
+echo $nombre;
+echo PHP_EOL;
+echo "Variable boolean:";
+echo $exite;
+?>
